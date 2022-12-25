4fb90b4e1df40f1a25cf8df72d06873c3e15936f
xcjpdx <1786355989@qq.com>
2022-12-25 11:13:48 +0800

    优化分页器MyPagination组件的代码

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -14,17 +14,21 @@
       v-for与v-if的优先级   面试题
       v-for的优先级高, 先执行, 每个遍历都会执行v-if
       1). 将v-if判断的处理放在v-for父标签上: 只需要判断一次(原本是每个遍历的元素都会判断)  ==> 适用于判断与元素无关的情况
-      2). 最好使用计算属性来去掉v-if  ===> 减少遍历的次数 ==> 适用于根据元素数据来判断的情况
-    -->
-    <!-- 
       <span v-if="isShow"> 
-        <button v-for="item in startEndArr"  :class="{active: item===myCurrentPage}" @click="setCurrentPage(item)">{{item}}</button>
+        <button
+        v-for="item in startEndArr"  
+        :class="{active: item===myCurrentPage}" 
+        @click="setCurrentPage(item)">{{item}}
+        </button>
       </span> 
+      2). 最好使用计算属性来去掉v-if  ===> 减少遍历的次数 ==> 适用于根据元素数据来判断的情况
+      <button 
+      v-for="item in startEnd.end" 
+      v-if="item>=startEnd.start" 
+      :class="{active: item===myCurrentPage}" 
+      @click="setCurrentPage(item)">{{item}}
+      </button>
     -->
-
-    <!-- <button v-for="item in startEnd.end" v-if="item>=startEnd.start" 
-      :class="{active: item===myCurrentPage}" @click="setCurrentPage(item)">{{item}}</button> -->
-
     <button
       v-for="(item, index) in startEndArr"
       :class="{ active: item === myCurrentPage }"
@@ -33,7 +37,6 @@
     >
       {{ item }}
     </button>
-
     <button disabled v-if="startEnd.end < totalPages - 1">···</button>
     <button
       v-if="startEnd.end < totalPages"
@@ -47,7 +50,6 @@
     >
       下一页
     </button>
-
     <button style="margin-left: 30px" disabled>共 {{ total }} 条</button>
   </div>
 </template>
@@ -55,19 +57,16 @@
 <script>
 export default {
   name: "MyPagination",
-
   props: {
     currentPage: {
       // 当前页码
       type: Number,
       default: 1,
-      // required: true
     },
     total: {
       // 总数量
       type: Number,
       default: 0,
-      // required: true
     },
     pageSize: {
       // 每页数量
@@ -83,7 +82,6 @@ export default {
       },
     },
   },
-
   data() {
     return {
       // 将传入当前页码作为内部的当前页码
@@ -91,15 +89,12 @@ export default {
       isShow: true,
     };
   },
-
   watch: {
     // 当接收到新的currentPage(本质就是Sesarch组件中的options.pageNo变化了)
     currentPage(value) {
-      // this.myCurrentPage = this.currentPage
       this.myCurrentPage = value;
     },
   },
-
   computed: {
     /* 
       总页数
@@ -108,24 +103,11 @@ export default {
       const { total, pageSize } = this; // 31 10 ==> 4
       return Math.ceil(total / pageSize); // 需要向上取整
     },
-
-    /* 
-      计算出包含从start到end的数组
-      */
-    startEndArr() {
-      const { start, end } = this.startEnd;
-      const arr = [];
-      for (let i = start; i <= end; i++) {
-        arr.push(i);
-      }
-      return arr;
-    },
-
     /* 
       计算出连续页码的start和end [3, 7]  {start: 3, end: 7}
       start最小值为1
       end最大值为totalPages
-      start到end的数量最大是showPageNo: 
+      start和end之间的关系可以用showPageNo表示出来: 
         end = start + showPageNo - 1
         start = end - showPageNo + 1
       */
@@ -146,7 +128,7 @@ export default {
           2                 5           8          1[2]345
         start上面算出的是0, 应该为1
         */
-      // 当当前页码比较小时, start值就会小于1, 小于了最小值
+      // 当 当前页码比较小时, start值就会小于1, 如果start值小于了最小值，那就将start的值修正为1
       if (start < 1) {
         start = 1;
       }
@@ -159,13 +141,12 @@ export default {
         start上面算出为1
         */
       end = start + showPageNo - 1; // 1 + 5 - 1
-
       //  end最大值为totalPages, 如果超过了, 修正为totalPages
       /* 
         myCurrentPage, showPageNo, totalPages
           8                5           9        567[8]9
-        start上面算出为 6  ==> 9 - 5 + 1 = 5
         end上面算出为  10  ==> 9
+        start上面算出为 6  ==> 9 - 5 + 1 = 5
         */
       if (end > totalPages) {
         // 修正end
@@ -189,8 +170,18 @@ export default {
         end,
       };
     },
+    /* 
+      计算出包含从start到end的数组
+      */
+    startEndArr() {
+      const { start, end } = this.startEnd;
+      const arr = [];
+      for (let i = start; i <= end; i++) {
+        arr.push(i);
+      }
+      return arr;
+    },
   },
-
   methods: {
     /* 
       设置新的当前页码
